5e349ecc6c752fb57416614eee80b00a76df89e3
Maya Mathur <mmathur@stanford.edu>
2023-05-16 06:26:52 -0700

    streamline Lodder analyses given sleek new pkg; woohoo

---
+++ .Rproj.user/4B98DAD8/sources/s-4C55ED85/6DAF8DB1-contents
@@ -0,0 +1,10 @@
+
+# "show rep res"
+# quickly look at results when running locally
+srr = function() {
+  cat("\n")
+  print( rep.res %>%
+           mutate_if(is.numeric, function(x) round(x,2)) )
+  cat("\n")
+  
+}
--- Lodder/Code/analyze_lodder.R
+++ Lodder/Code/analyze_lodder.R
@@ -6,35 +6,12 @@
 # next clean up this file and have it write the etahats, taus, etc.
 # YOU GOT THIS!
 
-# ~ Load Data and Packages -----------------------------------------------
 
-toLoad = c("crayon",
-           "dplyr",
-           "foreach",
-           "doParallel",
-           "boot",
-           "metafor",
-           "robumeta",
-           "data.table",
-           "purrr",
-           "metRology",
-           "fansi",
-           "MetaUtility",
-           "ICC",
-           "cfdecomp",
-           "tidyr",
-           "truncdist",
-           "tibble",
-           "tmvtnorm",
-           "testthat",
-           "truncreg",
-           "truncnorm",
-           "rstan",
-           "optimx",
-           "weightr",
-           "here",
-           "RColorBrewer",
-           "RoBMA")
+toLoad = c("crayon", "dplyr", "foreach", "doParallel", "boot", "metafor", 
+            "robumeta", "data.table", "purrr", "metRology", "fansi", "MetaUtility", 
+            "ICC", "cfdecomp", "tidyr", "truncdist", "tibble", "tmvtnorm", 
+            "testthat", "truncreg", "truncnorm", "rstan", "optimx", "weightr", 
+            "here", "RColorBrewer", "phacking")
 
 lapply( toLoad,
         require,
@@ -47,23 +24,15 @@ source("helper_SAPH.R")
 source("analyze_sims_helper_SAPH.R")
 
 # get prepped data
-setwd( here("Data and materials/Prepped data") )
+setwd( here("Lodder/Data and materials/Prepped data") )
 dp = fread("lodder_prepped.csv")
-
-# #@TEMP - try ROBMA
-# seconds = system.time(
-#   mod <<- RoBMA(d = dp$yi, se = sqrt(dp$vi), parallel = TRUE)
-# )
-# #@END TEMP
-
-seconds/60
+expect_equal( nrow(dp), 287 )  # this is the "Figure 1" analysis, not the "Appendix A" analysis 
 
 
 # for this script's own results
-results.dir = here("Results from R")
-overleaf.dir.figs = "/Users/mmathur/Dropbox/Apps/Overleaf/P-hacking (SAPH)/figures_SAPH/lodder"
-overleaf.dir.nums = "/Users/mmathur/Dropbox/Apps/Overleaf/P-hacking (SAPH)/results_from_R_SAPH"
-
+results.dir = here("Lodder/Results from R")
+overleaf.dir.figs = "/Users/mmathur/Dropbox/Apps/Overleaf/P-hacking (SAPH) Overleaf/figures_SAPH/lodder"
+overleaf.dir.nums = "/Users/mmathur/Dropbox/Apps/Overleaf/P-hacking (SAPH) Overleaf/results_from_R_SAPH"
 
 
 # MAKE DATA SUBSETS ------------------------------
@@ -82,9 +51,191 @@ dp.csm %>% group_by(Preregistered, affirm) %>%
 dp.prereg = dp %>% filter(Preregistered == TRUE)
 dpn.prereg = dpn %>% filter(Preregistered == TRUE)
 
-# INFO ABOUT DATASET  ------------------------------
 
-# ~ Number of studies of each type   ------------------------------
+# specify which methods to run, as in doParallel
+# but obviously can't run gold-std on a non-simulated meta-analysis
+all.methods = "naive ; maon ; pcurve ; 2psm ; rtma ; prereg-naive"
+# parse methods string
+all.methods = unlist( strsplit( x = all.methods,
+                                split = " ; " ) )
+
+
+
+# FIT MODELS  ------------------------------
+
+# initialize rep.res st run_method_safe and other standalone estimation fns
+#  will correctly recognize it as having 0 rows
+rep.res = data.frame()
+
+# ~~ Fit Naive Meta-Analysis (All PUBLISHED Draws) ------------------------------
+
+if ( "naive" %in% all.methods ) {
+  rep.res = run_method_safe(method.label = c("naive"),
+                            method.fn = function() {
+                              mod = rma( yi = dp$yi,
+                                         vi = dp$vi,
+                                         method = "REML",
+                                         knha = TRUE )
+                              
+                              report_meta(mod, .mod.type = "rma")
+                            },
+                            .rep.res = rep.res )
+  
+  Mhat.naive = rep.res$Mhat[ rep.res$method == "naive" ]
+  Shat.naive = rep.res$Shat[ rep.res$method == "naive" ]
+}
+
+srr()
+
+
+
+# ~~ Fit MAON (Nonaffirmative Published Draws) ------------------------------
+
+if ( "maon" %in% all.methods ) {
+  
+  rep.res = run_method_safe(method.label = c("maon"),
+                            method.fn = function() {
+                              mod = robu( yi ~ 1, 
+                                          data = dpn, 
+                                          studynum = 1:nrow(dpn),
+                                          var.eff.size = vi,
+                                          small = TRUE )
+                              
+                              report_meta(mod, .mod.type = "robu")
+                            },
+                            .rep.res = rep.res )
+  
+}
+
+
+srr()
+
+# ~~ Fit 2PSM (All Published Draws) ------------------------------
+
+if ( "2psm" %in% all.methods ) {
+  
+  rep.res = run_method_safe(method.label = c("2psm"),
+                            method.fn = function() {
+                              
+                              naive = rma( yi = dp$yi,
+                                           vi = dp$vi,
+                                           method = "REML",
+                                           knha = TRUE )
+                              
+                              mod = metafor::selmodel(naive, type="stepfun", alternative = "greater", steps=c(0.025, 1))
+                              
+                              stats = report_meta(mod, .mod.type = "rma")
+                              
+                              stats$stats$Eta = 1/mod$delta[2]
+                              stats$stats$EtaLo = 1/mod$ci.ub.delta[2]
+                              stats$stats$EtaHi = 1/mod$ci.lb.delta[2]
+                              
+                              stats
+                            },
+                            .rep.res = rep.res )
+  
+}
+
+srr()
+
+
+# ~~ Fit P-Curve (Published Affirmatives) ------------------------------
+
+if ( "pcurve" %in% all.methods ) {
+  # since pcurve.opt internally retains only 
+  rep.res = run_method_safe(method.label = c("pcurve"),
+                            method.fn = function() {
+                              #@later, revisit the decision to use df_obs = 1000
+                              #   to effectively treat yi/sei z-scores
+                              
+                              # pass all significant studies (that's what pcurve.opt does internally):
+                              Mhat = pcurve.opt( t_obs = dp$Zi,
+                                                 df_obs = rep(1000, length(dp$Zi)),
+                                                 dmin = -5, #@HARD-CODED and arbitrary
+                                                 dmax = 5)
+                              
+                              
+                              # # using only published affirmatives:
+                              # Mhat = pcurve.opt( t_obs = dpa$yi/dpa$sei,
+                              #                    df_obs = rep(1000, length(dpa$yi)),
+                              #                    dmin = -5, #@HARD-CODED and arbitrary
+                              #                    dmax = 5)
+                              
+                              
+                              return( list( stats = data.frame( Mhat = Mhat) ) )
+                            },
+                            .rep.res = rep.res )
+  
+}
+
+# ~~ RTMA ------------------------------
+
+if ( "rtma" %in% all.methods ) {
+  # # temp for refreshing code
+  # path = "/home/groups/manishad/SAPH"
+  # setwd(path)
+  # source("helper_SAPH.R")
+  
+  # this one has two labels in method arg because a single call to estimate_jeffreys_mcmc
+  #  returns 2 lines of output, one for posterior mean and one for posterior median
+  # order of labels in method arg needs to match return structure of estimate_jeffreys_mcmc
+  rep.res = run_method_safe(method.label = "rtma",
+                            method.fn = function() {
+                              
+                              mod = phacking_meta(yi = dp$yi,
+                                                  sei = sqrt(dp$vi))
+                              
+                              # follow the same return structure as report_meta
+                              list( stats = data.frame( Mhat = mod$stats$mode[1],
+                                                        MLo = mod$stats$ci_lower[1],
+                                                        MHi = mod$stats$ci_upper[1],
+                                                        
+                                                        Shat = mod$stats$mode[2],
+                                                        SLo = mod$stats$ci_lower[2],
+                                                        SHi = mod$stats$ci_upper[2] ) ) 
+                              
+                              
+                            },
+                            .rep.res = rep.res )
+  
+  cat("\n doParallel flag: Done rtma if applicable")
+}
+
+cat("\n")
+rep.res
+cat("\n")
+
+
+
+# ~~ Fit Naive (Prereg Only) ------------------------------
+
+if ( "prereg-naive" %in% all.methods ) {
+  rep.res = run_method_safe(method.label = c("prereg-naive"),
+                            method.fn = function() {
+                              mod = rma( yi = dp.prereg$yi,
+                                         vi = dp.prereg$vi,
+                                         method = "REML",
+                                         knha = TRUE )
+                              
+                              report_meta(mod, .mod.type = "rma")
+                            },
+                            .rep.res = rep.res )
+  
+  Mhat.naive = rep.res$Mhat[ rep.res$method == "prereg-naive" ]
+  Shat.naive = rep.res$Shat[ rep.res$method == "prereg-naive" ]
+}
+
+srr()
+
+
+
+# ADD BASIC DATASET INFO TO RESULTS ------------
+
+rep.res$k.pub = nrow(dp)
+rep.res$k.pub.affirm = sum(dp$affirm == TRUE)
+rep.res$k.pub.nonaffirm = sum(dp$affirm == FALSE)
+rep.res$prob.pub.study.affirm = rep.res$k.pub.affirm / rep.res$k.pub
+
 
 update_result_csv( name = "Lodder k total",
                    value = nrow(dp),
@@ -121,6 +272,15 @@ update_result_csv( name = "Lodder k affirm",
 update_result_csv( name = "Lodder k nonaffirm",
                    value = sum(dp$affirm == 0),
                    .overleaf.dir = overleaf.dir.nums )
+
+
+# ~ WRITE LONG RESULTS ------------------------------
+
+setwd(results.dir)
+fwrite( rep.res, "results_lodder.csv" )
+
+
+
 
 # Z-SCORE DENSITY  ------------------------------
 
@@ -220,7 +380,6 @@ p = ggplot(data = data.frame(x = c(xmin, 3)),
   theme( text = element_text(size = 16, face = "bold"),
          panel.grid.major.y = element_blank(),
          panel.grid.minor.y = element_blank() )
-#bm
 
 
 setwd(results.dir)
@@ -234,8 +393,8 @@ my_ggsave( name = paste( "lodder_z_density_by_prereg.pdf", sep="_" ),
 
 # QQ PLOT FOR RTMA FIT  ------------------
 
-Mhat = rs$Mhat[ rs$method == "jeffreys-mcmc-pmed" ]
-Shat = rs$Shat[ rs$method == "jeffreys-mcmc-pmed" ]
+Mhat = rep.res$Mhat[ rep.res$method == "rtma" ]
+Shat = rep.res$Shat[ rep.res$method == "rtma" ]
 
 p = yi_qqplot(yi = dpn$yi,
               sei = dpn$sei,
@@ -258,19 +417,18 @@ my_ggsave( name = paste( "lodder_rtma_qqplot.pdf", sep="_" ),
 # ~ Recode method ------------------------
 
 # only name the methods we're going to show in paper
-rs$method.pretty = NA
-rs$method.pretty[ rs$method == c("naive") ] = "Uncorrected"
-rs$method.pretty[ rs$method == c("maon") ] = "MAN"
-rs$method.pretty[ rs$method == c("2psm") ] = "SM"
-#rs$method.pretty[ rs$method == c("2psm-csm-dataset") ] = "SMKH" # "known hacking"
-rs$method.pretty[ rs$method == c("prereg-naive") ] = "Preregistered only"
-rs$method.pretty[ rs$method %in% c("jeffreys-mcmc-pmed") ] = "RTMA"
-table(rs$method, rs$method.pretty)
+rep.res$method.pretty = NA
+rep.res$method.pretty[ rep.res$method == c("naive") ] = "Uncorrected"
+rep.res$method.pretty[ rep.res$method == c("maon") ] = "MAN"
+rep.res$method.pretty[ rep.res$method == c("2psm") ] = "SM"
+rep.res$method.pretty[ rep.res$method == c("prereg-naive") ] = "Preregistered only"
+rep.res$method.pretty[ rep.res$method %in% c("rtma") ] = "RTMA"
+table(rep.res$method, rep.res$method.pretty)
 
 
 
 # ~ Plotting dataframe ----------------------
-rsp = rs %>% filter( !is.na(method.pretty) &
+rsp = rep.res %>% filter( !is.na(method.pretty) &
                        # I accidentally included 2 rows for naive
                        !duplicated(method.pretty) ) %>%
   droplevels()
@@ -386,8 +544,7 @@ my_ggsave( name = paste( "lodder_forest.pdf", sep="_" ),
 
 # WRITE INDIVIDUAL NUMERICAL ESTIMATES ------------------
 
-# only keep certain methods
-
+# Mhat and CI limits
 update_result_csv( name = paste( "Lodder", rsp$method.pretty, "Mhat", sep = " "),
                    value = round( rsp$Mhat, 2),
                    .overleaf.dir = overleaf.dir.nums )
@@ -401,8 +558,32 @@ update_result_csv( name = paste( "Lodder", rsp$method.pretty, "MHi", sep = " "),
                    value = round( rsp$MHi, 2),
                    .overleaf.dir = overleaf.dir.nums )
 
+# Shat and CI limits
+# note: MAN has no CI for Shat because robu doesn't provide one
+update_result_csv( name = paste( "Lodder", rsp$method.pretty, "Shat", sep = " "),
+                   value = round( rsp$Shat, 2),
+                   .overleaf.dir = overleaf.dir.nums )
 
+update_result_csv( name = paste( "Lodder", rsp$method.pretty, "SLo", sep = " "),
+                   value = round( rsp$SLo, 2),
+                   .overleaf.dir = overleaf.dir.nums )
 
+update_result_csv( name = paste( "Lodder", rsp$method.pretty, "SHi", sep = " "),
+                   value = round( rsp$SHi, 2),
+                   .overleaf.dir = overleaf.dir.nums )
 
+# EtaHat for SM
+temp = rsp %>% filter(method == "2psm")
+update_result_csv( name = paste( "Lodder", temp$method.pretty, "EtaHat", sep = " "),
+                   value = round( temp$Eta, 2),
+                   .overleaf.dir = overleaf.dir.nums )
+
+update_result_csv( name = paste( "Lodder", temp$method.pretty, "EtaLo", sep = " "),
+                   value = round( temp$EtaLo, 2),
+                   .overleaf.dir = overleaf.dir.nums )
+
+update_result_csv( name = paste( "Lodder", temp$method.pretty, "EtaHi", sep = " "),
+                   value = round( temp$EtaHi, 2),
+                   .overleaf.dir = overleaf.dir.nums )
 
 
